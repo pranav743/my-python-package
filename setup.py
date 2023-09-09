5cd7d78d69e47f12430adc2fab50069dc7e65892
Pranav Patil <pranav07@somaiya.edu>
2023-09-09 15:35:52 +0530

    Updated version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pranav",
-    version="0.1.0",
+    version="0.0.1",
     description="A package for colorized printing and list printing",
     author="Pranav Patil",
     author_email="your.email@example.com",
